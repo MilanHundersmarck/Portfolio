ee2bc88a8ab1f5700ca42dd0203f3cb4c6cb00fe
MilanHundersmarck <milanhun1@hotmail.com>
2025-04-03 16:32:53 +0200

    First draft of portfolio

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,18 +1,24 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const projectsCollection = defineCollection({
-  type: 'content',
+  type: "content",
   schema: z.object({
     title: z.string(),
     description: z.string(),
-    images: z.array(z.object({
-      url: z.string(),
-      alt: z.string()
-    })).optional(),
-    highlighted: z.boolean().optional().default(false)
-  })
+    images: z
+      .array(
+        z.object({
+          url: z.string(),
+          alt: z.string(),
+        }),
+      )
+      .optional(),
+    highlighted: z.boolean().optional().default(false),
+    href: z.string().optional(),
+    visitable: z.boolean().optional().default(true),
+  }),
 });
 
 export const collections = {
-  'projects': projectsCollection
-}; 
+  projects: projectsCollection,
+};
---
+++ src/content/projects/coming-soon.md
@@ -0,0 +1,12 @@
+---
+title: "Coming soon"
+description: "My projects will soon be available."
+images:
+  - url: "/projects/coming-soon/image1.png"
+    alt: "An example image"
+  - url: "/projects/coming-soon/image2.png"
+    alt: "Another example image"
+highlighted: true
+visitable: false
+href: "/"
+---
